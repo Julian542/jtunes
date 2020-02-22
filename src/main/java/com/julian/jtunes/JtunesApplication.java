228a88e49b79ff85790d8764480018a5da926e67
Julian542 <waltergc248@gmail.com>
2020-02-22 19:59:58 -0300

    initial commit

---
+++ src/main/java/com/julian/jtunes/JtunesApplication.java
@@ -0,0 +1,13 @@
+package com.julian.jtunes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtunesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtunesApplication.class, args);
+	}
+
+}
