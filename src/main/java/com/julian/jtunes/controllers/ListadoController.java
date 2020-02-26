b5a10721ebd99d7a7bdf047a3da1af0332768617
Julian542 <waltergc248@gmail.com>
2020-02-26 03:46:04 -0300

    Creacion del 1er DTO y del 1er Service, implementacion del metodo findFeaturedSongs

--- src/main/java/com/julian/jtunes/controllers/ListadoController.java
+++ src/main/java/com/julian/jtunes/controllers/ListadoController.java
@@ -1,10 +1,17 @@
 package com.julian.jtunes.controllers;
 
+import com.julian.jtunes.services.SongService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ListadoController {
+    
+    private final SongService songService;
+    
+    public ListadoController( SongService songService){
+        this.songService = songService;
+    }
     
     @RequestMapping("/")
     public String listarCanciones(){
